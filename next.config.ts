c1af428634ba91c097d9101d6340ba93a595b3b3
rudi <yrudikov@gmail.com>
2025-03-25 19:52:29 +0100

    Update next.config

--- next.config.ts
+++ next.config.ts
@@ -1,14 +1,7 @@
 import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
-    output: 'export',
 
-    basePath: "/frontend-challenge",
-    assetPrefix: "/frontend-challenge/",
-    reactStrictMode: true,
-    images: {
-        unoptimized: true,
-    }
 };
 
 export default nextConfig;
