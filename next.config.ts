a045ae0077af684e96af52598e0ade86feebf254
yrudikov <89663152+yrudikov@users.noreply.github.com>
2025-03-25 19:00:22 +0100

    Update next.config.ts

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@ import type { NextConfig } from "next";
 
 const nextConfig: NextConfig = {
     output: 'export',
-    basePath: process.env.PAGES_BASE_PATH,
+    basePath: "/frontend-challenge"
 };
 
 export default nextConfig;
