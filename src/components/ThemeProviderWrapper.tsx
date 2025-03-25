eed4464a6a584233a2df82668d0f7021d5821cc0
rudi <yrudikov@gmail.com>
2025-03-25 12:48:36 +0100

    Resolved Hydration error

--- src/components/ThemeProviderWrapper.tsx
+++ src/components/ThemeProviderWrapper.tsx
@@ -1,14 +1,18 @@
 'use client';
 
-import { ThemeProvider } from 'next-themes';
+import {ThemeProvider} from 'next-themes';
 import React, {memo} from "react";
+import dynamic from "next/dynamic";
 
-const ThemeProviderWrapper = ({ children }: { children: React.ReactNode })=> {
-    return (
-        <ThemeProvider attribute="class" defaultTheme="system">
-            {children}
-        </ThemeProvider>
-    );
-}
+const ThemeProviderWrapper = dynamic(() => {
+    return Promise.resolve(({children}: { children: React.ReactNode }) => {
+        return (
+            <ThemeProvider attribute="class" defaultTheme="system">
+                {children}
+            </ThemeProvider>
+        );
+    })
+}, {ssr: false});
 
-export default memo(ThemeProviderWrapper);
+    export default memo(ThemeProviderWrapper);
+
